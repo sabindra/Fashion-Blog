bee8f018697b35388f23b82745a37692a6d927a3
Rajesh Basnet <basnerahzes@hotmail.com>
2016-10-31 20:10:52 +1100

    partials structured

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 
 $db['db_host']="localhost";
 $db['db_name']="fashion_blog_2016";
-$db['user']="ryan@reb";
+$db['user']="ryan@web";
 $db['pass']="RajRadha88";
 
 
--- index.php
+++ index.php
@@ -1,56 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-		<meta name="description" content="">
-		<meta name="author" content="">
-		<link rel="icon" href="favicon.ico">
-		<title>Renda - clean blog theme based on Bootstrap</title>
-		<!-- Bootstrap core CSS -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-		<!-- Custom styles for this template -->
-		<link href="css/jquery.bxslider.css" rel="stylesheet">
-		<link href="css/style.css" rel="stylesheet">
-	</head>
+<?php
+	 include("config/db.php");
+	$query = $dbh->prepare("SELECT * FROM categories");
+	$query->execute();
+
+			$categories = $query->fetchAll(PDO::FETCH_COLUMN,1);
+			foreach	($categories as $item){
+
+			$category[] = $item;
+			}
+			sort($category);
+	 include("partials/header.php");
+
+	?>
 	<body>
 		<!-- Navigation -->
-		<nav class="navbar navbar-inverse navbar-fixed-top">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					</button>
-				</div>
-				<div id="navbar" class="collapse navbar-collapse">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="index.php">Home</a></li>
-						<li><a href="#about">Lifestyle</a></li>
-						<li><a href="#contact">Travel</a></li>
-						<li><a href="#contact">Fashion</a></li>
-						<li><a href="about.html">About Me</a></li>
-						<li><a href="about.html">Contact</a></li>
-					</ul>
-
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="#"><i class="fa fa-instagram"></i></a></li>
-						<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-						<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-						<li><a href="#"><i class="fa fa-reddit"></i></a></li>
-					</ul>
-
-				</div>
-				<!--/.nav-collapse -->
-			</div>
-		</nav>
+		<?php include("partials/navigation.php") ?>
 
 		<div class="container">
 		<header>
@@ -104,129 +68,16 @@
 				</div>
 				<div class="col-md-4 sidebar-gutter">
 					<aside>
-					<!-- sidebar-widget -->
-					<div class="sidebar-widget">
-						<h3 class="sidebar-title">About Me</h3>
-						<div class="widget-container widget-about">
-							<a href="post.html"><img src="images/author.jpg" alt=""></a>
-							<h4>Jamie Mooz</h4>
-							<div class="author-title">Designer</div>
-							<p>While everyone’s eyes are glued to the runway, it’s hard to ignore that there are major fashion moments on the front row too.</p>
-						</div>
-					</div>
-					<!-- sidebar-widget -->
-					<div class="sidebar-widget">
-						<h3 class="sidebar-title">Featured Posts</h3>
-						<div class="widget-container">
-							<article class="widget-post">
-								<div class="post-image">
-									<a href="post.html"><img src="images/90x60-1.jpg" alt=""></a>
-								</div>
-								<div class="post-body">
-									<h2><a href="post.html">The State of the Word 2014</a></h2>
-									<div class="post-meta">
-										<span><i class="fa fa-clock-o"></i> 2. august 2015</span> <span><a href="post.html"><i class="fa fa-comment-o"></i> 23</a></span>
-									</div>
-								</div>
-							</article>
-							<article class="widget-post">
-								<div class="post-image">
-									<a href="post.html"><img src="images/90x60-2.jpg" alt=""></a>
-								</div>
-								<div class="post-body">
-									<h2><a href="post.html">Why The Muppets Needs to Channel 30 Rock</a></h2>
-									<div class="post-meta">
-										<span><i class="fa fa-clock-o"></i> 2. august 2015</span> <span><a href="post.html"><i class="fa fa-comment-o"></i> 23</a></span>
-									</div>
-								</div>
-							</article>
-							<article class="widget-post">
-								<div class="post-image">
-									<a href="post.html"><img src="images/90x60-3.jpg" alt=""></a>
-								</div>
-								<div class="post-body">
-									<h2><a href="post.html">The State of the Word 2014</a></h2>
-									<div class="post-meta">
-										<span><i class="fa fa-clock-o"></i> 2. august 2015</span> <span><a href="post.html"><i class="fa fa-comment-o"></i> 23</a></span>
-									</div>
-								</div>
-							</article>
-							<article class="widget-post">
-								<div class="post-image">
-									<a href="post.html"><img src="images/90x60-4.jpg" alt=""></a>
-								</div>
-								<div class="post-body">
-									<h2><a href="post.html">Vintage-Inspired Finds for Your Home</a></h2>
-									<div class="post-meta">
-										<span><i class="fa fa-clock-o"></i> 2. august 2015</span> <span><a href="post.html"><i class="fa fa-comment-o"></i> 23</a></span>
-									</div>
-								</div>
-							</article>
-							<article class="widget-post">
-								<div class="post-image">
-									<a href="post.html"><img src="images/90x60-5.jpg" alt=""></a>
-								</div>
-								<div class="post-body">
-									<h2><a href="post.html">The State of the Word 2014</a></h2>
-									<div class="post-meta">
-										<span><i class="fa fa-clock-o"></i> 2. august 2015</span> <span><a href="post.html"><i class="fa fa-comment-o"></i> 23</a></span>
-									</div>
-								</div>
-							</article>
-						</div>
-					</div>
-					<!-- sidebar-widget -->
-					<div class="sidebar-widget">
-						<h3 class="sidebar-title">Socials</h3>
-						<div class="widget-container">
-							<div class="widget-socials">
-								<a href="#"><i class="fa fa-facebook"></i></a>
-								<a href="#"><i class="fa fa-twitter"></i></a>
-								<a href="#"><i class="fa fa-instagram"></i></a>
-								<a href="#"><i class="fa fa-google-plus"></i></a>
-								<a href="#"><i class="fa fa-dribbble"></i></a>
-								<a href="#"><i class="fa fa-reddit"></i></a>
-							</div>
-						</div>
-					</div>
-					<!-- sidebar-widget -->
-					<div class="sidebar-widget">
-						<h3 class="sidebar-title">Categories</h3>
-						<div class="widget-container">
-							<ul>
-								<li><a href="#">Fashion</a></li>
-								<li><a href="#">Art</a></li>
-								<li><a href="#">Design</a></li>
-								<li><a href="#">Featured</a></li>
-								<li><a href="#">Graphics</a></li>
-								<li><a href="#">Peoples</a></li>
-								<li><a href="#">Uncategorized</a></li>
-							</ul>
-						</div>
-					</div>
-					</div>
+					<?php include("partials/sidebar.php") ?>
 					</aside>
 				</div>
 			</div>
 		</section>
 		</div><!-- /.container -->
 
-		<footer class="footer">
+		<!-- Footer -->
 
-			<div class="footer-socials">
-				<a href="#"><i class="fa fa-facebook"></i></a>
-				<a href="#"><i class="fa fa-twitter"></i></a>
-				<a href="#"><i class="fa fa-instagram"></i></a>
-				<a href="#"><i class="fa fa-google-plus"></i></a>
-				<a href="#"><i class="fa fa-dribbble"></i></a>
-				<a href="#"><i class="fa fa-reddit"></i></a>
-			</div>
-
-			<div class="footer-bottom">
-				<i class="fa fa-copyright"></i> Copyright 2015. All rights reserved.<br>
-				Theme made by <a href="http://www.moozthemes.com">MOOZ Themes</a>
-			</div>
-		</footer>
+		<?php include("partials/footer.php") ?>
 
 		<!-- Bootstrap core JavaScript
 			================================================== -->
