93eb7fcf061e1c2400a570f5acb0cf3ffeb826be
Rajesh Basnet <basnerahzes@hotmail.com>
2016-11-01 23:23:43 +1100

    post update

--- admin/partials/add_post.php
+++ admin/partials/add_post.php
@@ -1,21 +1,51 @@
 <?php 
+if(isset($_POST['submit'])){
+$title =$_POST['title'];
+$title =$_POST['content'];
+$title =$_POST['category'];
+$title =$_POST['tag'];
+$title =$_POST['status'];
+$author =$_POST['author'];
+$date = date('d-m-Y');
+echo $date;
 
 
+// file upload
+ $image = $_FILES['post_image']['name'];
+ $image_size = $_FILES['post_image']['size'];
+ $image_temp = $_FILES['post_image']['tmp_name'];
+ if($_FILES['post_image']['name'])
+{
+ echo $image_size;
+ echo $_FILES['post_image']['name'];
+ echo $_FILES['post_image']['type'];
+}
+}
 ?>
 
-<form>
+<form method="post" enctype="multipart/form-data">
   <div class="form-group">
-    <label for="email">Email address:</label>
+    <label for="title">Title:</label>
+    <input type="text" class="col-xs-6 form-control" id="title" name="title">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Content:</label>
+    <textarea type="text" class="form-control" id="content" name="content" rows="15"></textarea>
+    <div class="form-group">
+    <label for="email">Image</label>
+    <input type="file" class="" id="email" name="post_image">
+  </div>
+  </div>
+  <div class="form-group">
+    <label for="email">tags:</label>
+    <input type="email" class="form-control" id="image" name="tag">
+  </div>
+  <div class="form-group">
+    <label for="email">status:</label>
     <input type="email" class="form-control" id="email">
   </div>
-  <div class="form-group">
-    <label for="pwd">Password:</label>
-    <input type="password" class="form-control" id="pwd">
-  </div>
-  <div class="checkbox">
-    <label><input type="checkbox"> Remember me</label>
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
+ 
+  <button type="submit" class="btn btn-default" name="submit">Submit</button>
 </form>
 
 
