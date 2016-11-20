1588e4b0a9522785185b4a16ea830e2df6260ff0
Ryan Basnet <basnetrahzes@hotmail.com>
2016-11-20 23:01:47 +1100

    CsrfMiddleware added

--- cms/app/Middleware/CsrfMiddleware.php
+++ cms/app/Middleware/CsrfMiddleware.php
@@ -17,14 +17,14 @@ class CsrfMiddleware extends Middleware{
 
 	public function __invoke($request,$response,$next){
 		$csrf = array();
-
+		//generate csrf raw html input fiels for post request
 		$csrf['field'] = '<input type="hidden" name="'.$this->container->csrf->getTokenNameKey().'" 
 						  value = "'.$this->container->csrf->getTokenName() .'">
+
 						  <input type="hidden" name="'.$this->container->csrf->getTokenValueKey().'" 
 						  value = "'.$this->container->csrf->getTokenValue() .'">';
 	
 		$this->container->view->getEnvironment()->addGlobal('csrf',$csrf);
-
 
 		$response =$next($request,$response);
 		
