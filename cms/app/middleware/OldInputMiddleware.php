3c06cabbaca42f7498d281cb90d8444db3fb3870
Ryan Basnet <basnetrahzes@hotmail.com>
2016-11-21 11:44:27 +1100

    old data persist middleware added

---
+++ cms/app/middleware/OldInputMiddleware.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Middleware;
+
+
+class OldInputMiddleware extends Middleware{
+
+
+
+public function __invoke($request,$response,$next){
+
+$_SESSION['old']= $request->getParams();
+$this->container->view->getEnvironment()->addGlobal('oldInput',$_SESSION['old']);
+
+
+
+$response = $next($request,$response);
+return $response;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
